ba1910380cacc0c014dfe43f9885eff3e03a9569
Jennifer <jen1992lo@gmail.com>
2024-03-24 16:33:25 +0100

    Actualizacion de maquetacion en dark mode

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -7,9 +7,9 @@ const WelcomePage = () => {
   return (
     <div className=" w-full  h-[65vh] md:flex md:justify-center md:items-start">
       <div className=" mt-10 flex flex-col content-center items-center ">
-        <h1 className="text-3xl text-center text-gray-400" > Hola {user ? user.email : 'Jedi'}
+        <h1 className="text-3xl text-center text-gray-900 dark:text-inherit" > Hola {user ? user.email : 'Jedi'}
         </h1>
-          <h2 className="text-xl my-5 text-center">Bienvenido/a a la pagina de Naves espaciales de StarWars</h2>
+          <h2 className="text-xl my-5 text-center text-gray-900 dark:text-inherit">Bienvenido/a a la pagina de Naves espaciales de StarWars</h2>
           <img className="w-1/2 " src="/storm-trooper-star-wars.gif" alt="" />
       </div>
     </div>
